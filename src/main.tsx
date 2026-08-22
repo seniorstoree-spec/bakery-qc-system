66280c8d4308b39581c52a4cd89308985ffd1065
mustasharik <seniorstoree@gmail.com>
2026-08-22 13:22:47 +0300

    feat: enable global numeric input normalization

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { installGlobalInputNormalization } from './utils/normalizeDigits';
+
+installGlobalInputNormalization();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
