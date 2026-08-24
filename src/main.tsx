e46446c67a23564d91d35fd8b730679fc0b90a4d
mustasharik <seniorstoree@gmail.com>
2026-08-24 18:49:55 +0300

    Install IPC compliance UI controls

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import App from './App';
 import './index.css';
 import './desktop-responsive.css';
 import { installGlobalInputNormalization } from './utils/normalizeDigits';
+import { installIpcComplianceControls } from './utils/ipcComplianceControls';
 
 installGlobalInputNormalization();
+installIpcComplianceControls();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
